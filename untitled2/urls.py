e7154a593358556086bed837bfe93d701abf1e4f
AnGherase <56508394+AnGherase@users.noreply.github.com>
2020-07-26 18:52:33 +0300

    add products and categories

--- untitled2/urls.py
+++ untitled2/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('', include('shop.urls', namespace='shop')),
     path('blog/', include('blog.urls', namespace='blog')),
 ]
